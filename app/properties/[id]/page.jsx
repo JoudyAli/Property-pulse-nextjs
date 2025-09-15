70ebb2f4404d0e7f6e2e3ab17699202e82d74d79
Joudy <joudyali.de@gmail.com>
2025-09-15 12:06:36 +0200

    demo: added usePathname() to show current URL path (course example)

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,11 +1,17 @@
 "use client";
-import { useRouter, useParams, useSearchParams } from "next/navigation";
+import {
+  useRouter,
+  useParams,
+  useSearchParams,
+  usePathname,
+} from "next/navigation";
 const PropertyPage = () => {
   const router = useRouter();
   const params = useParams();
   const searchParams = useSearchParams();
+  const pathName = usePathname();
 
-  return <div>Property Page {searchParams.get("name")}</div>;
+  return <div>Property Page {pathName}</div>;
 };
 
 export default PropertyPage;
