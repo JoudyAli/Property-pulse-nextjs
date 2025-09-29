fe6fa9f6219dfb4f13e0e7612715125e75e22935
Joudy <joudyali.de@gmail.com>
2025-09-29 18:08:46 +0200

    Update Property Action

---
+++ app/actions/updateProperty.js
@@ -0,0 +1,59 @@
+"use server";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+async function updateProperty(properyId, formData) {
+  await connectDB();
+  const sessionUser = await getSessionUser();
+
+  if (!sessionUser || !sessionUser.userId) {
+    throw new Error("User ID is required");
+  }
+
+  const { userId } = sessionUser;
+
+  const existingProperty = await Property.findById(properyId);
+  // Verify ownership
+  if (existingProperty.owner.toString() !== userId) {
+    throw new Error("Current user dose not own this property");
+  }
+
+  const propertyData = {
+    owner: userId,
+    type: formData.get("type"),
+    name: formData.get("name"),
+    description: formData.get("description"),
+    location: {
+      street: formData.get("location.street"),
+      city: formData.get("location.city"),
+      state: formData.get("location.state"),
+      zipcode: formData.get("location.zipcode"),
+    },
+    beds: formData.get("beds"),
+    baths: formData.get("baths"),
+    square_feet: formData.get("square_feet"),
+    amenities: formData.getAll("amenities"),
+    rates: {
+      nightly: formData.get("rates.nightly"),
+      weekly: formData.get("rates.weekly"),
+      monthly: formData.get("rates.monthly"),
+    },
+    seller_info: {
+      name: formData.get("seller_info.name"),
+      email: formData.get("seller_info.email"),
+      phone: formData.get("seller_info.phone"),
+    },
+  };
+
+  const updateProperty = await Property.findByIdAndUpdate(
+    properyId,
+    propertyData
+  );
+  revalidatePath("/", "layout");
+  redirect(`/properties/${updateProperty._id}`);
+}
+
+export default updateProperty;
