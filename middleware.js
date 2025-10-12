a402b53275a6ffd02feb66c627d7187dfc67f868
Joudy <joudyali.de@gmail.com>
2025-10-12 15:38:31 +0200

    remove middleware test and add back logs

--- middleware.js
+++ middleware.js
@@ -1,21 +1,8 @@
-import { withAuth } from "next-auth/middleware";
-
-console.log(
-  "🧩 NEXTAUTH_SECRET available in Edge:",
-  !!process.env.NEXTAUTH_SECRET
-);
-console.log("🧩 NEXTAUTH_URL:", process.env.NEXTAUTH_URL);
-
-export default withAuth(
-  function middleware(req) {
-    return;
+export default withAuth({
+  callbacks: {
+    authorized: ({ token }) => !!token,
   },
-  {
-    callbacks: {
-      authorized: ({ token }) => !!token,
-    },
-  }
-);
+});
 
 export const config = {
   matcher: ["/properties/add", "/profile", "/properties/saved", "/messages"],
