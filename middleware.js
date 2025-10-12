e69cd3978515d263b0f47d4a559cb6d79bbd8f66
Joudy <joudyali.de@gmail.com>
2025-10-12 15:19:52 +0200

    revert adding logs

--- middleware.js
+++ middleware.js
@@ -1,22 +1,4 @@
-import { withAuth } from "next-auth/middleware";
-
-export default withAuth(
-  function middleware(req) {
-    console.log("Middleware triggered for:", req.nextUrl.pathname);
-    console.log("Token exists:", !!req.nextauth.token);
-    console.log("User:", req.nextauth.token?.email);
-  },
-  {
-    callbacks: {
-      authorized: ({ req, token }) => {
-        console.log("Authorization check for:", req.nextUrl.pathname);
-        console.log("Token present:", !!token);
-        console.log("NEXTAUTH_URL:", process.env.NEXTAUTH_URL);
-        return !!token;
-      },
-    },
-  }
-);
+export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: ["/properties/add", "/profile", "/properties/saved", "/messages"],
